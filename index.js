fa6b8f3e42685afb6864cad5d434b6a44257f79e
Robert Wagner <rwwagner90@gmail.com>
2018-02-05 20:55:08 -0500

    Add message when no baseRoot is defined

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const chalk = require('chalk');
 const generateSitemap = require('./lib/sitemap-generator').generateSitemap;
 
 module.exports = {
@@ -8,6 +8,11 @@ module.exports = {
   postBuild() {
     const premberOptions = this.app.options['prember'];
     const baseRoot = premberOptions.baseRoot;
+    if (!baseRoot) {
+      return this.ui.writeLine(chalk.red(
+        'ERROR: You must define `baseRoot` for prember-sitemap-generator to generate sitemaps.'
+      ));
+    }
     const urls = premberOptions && premberOptions.urls ? premberOptions.urls : [];
     generateSitemap(baseRoot, urls);
   }
