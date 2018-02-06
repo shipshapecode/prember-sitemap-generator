c8e7135fc9adb18f90ea1a832b262edc4bee0922
Robert Wagner <rwwagner90@gmail.com>
2018-02-05 23:18:15 -0500

    Use treeForPublic

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 'use strict';
+const Funnel = require('broccoli-funnel');
 const chalk = require('chalk');
 const generateSitemap = require('./lib/sitemap-generator').generateSitemap;
+const writeFile = require('broccoli-file-creator');
 
 module.exports = {
   name: 'prember-sitemap-generator',
 
-  postBuild() {
-    const premberOptions = this.app.options['prember'];
-    const baseRoot = premberOptions.baseRoot;
-    if (!baseRoot) {
-      return this.ui.writeLine(chalk.red(
-        'ERROR: You must define `baseRoot` for prember-sitemap-generator to generate sitemaps.'
-      ));
+  treeForPublic() {
+    this._super.treeForPublic && this._super.treeForPublic.apply(this, arguments);
+
+    if (process.env.EMBER_ENV === 'production') {
+      const premberOptions = this.app.options['prember'];
+      const baseRoot = premberOptions.baseRoot;
+      if (!baseRoot) {
+        return this.ui.writeLine(chalk.red(
+          'ERROR: You must define `baseRoot` for prember-sitemap-generator to generate sitemaps.'
+        ));
+      }
+      const urls = premberOptions && premberOptions.urls ? premberOptions.urls : [];
+      const tree = writeFile('/prember-sitemap-generator/sitemap.xml', generateSitemap(baseRoot, urls));
+
+      return new Funnel(tree, {
+        srcDir: 'prember-sitemap-generator'
+      });
     }
-    const urls = premberOptions && premberOptions.urls ? premberOptions.urls : [];
-    generateSitemap(baseRoot, urls);
   }
 };
